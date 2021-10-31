71b9ebaed1d6ebbf440f5e0921fe2afac811905f
Ryota Fujishima <ry.fj0304@gmail.com>
2021-10-31 23:52:36 +0900

    echoバージョン変更

--- back/server.go
+++ back/server.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
@@ -12,6 +12,6 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World!")
 	})
-	
+
 	e.Logger.Fatal(e.Start(":3000"))
 }
